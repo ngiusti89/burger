88cd12ec3fc116f69c894a6b05d217706635d5d8
Natasha Giusti <natashagiusti@tashs-mac.lan>
2019-06-01 22:35:27 -0700

    cleaned up, still getting error

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,9 @@ var router = express.Router();
 
 router.get("/", function (req, res) {
     burger.selectAll(function (data) {
-        var handlebarsObject = { burgers: data };
+        var handlebarsObject = {
+            burgers: data
+        };
         console.log(handlebarsObject);
         res.render("index", handlebarsObject);
     });
