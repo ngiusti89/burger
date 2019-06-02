a0bc397554ddd3f0aec1f3fc80d61a0f2dda6b4f
Natasha Giusti <natashagiusti@tashs-mac.lan>
2019-06-01 21:31:26 -0700

    added put/update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,14 +10,26 @@ router.get("/", function (req, res) {
     });
 });
 
-router.post("/api/burger", function (req, res) {
+router.post("/", function (req, res) {
     burger.insertOne([
         "burger_name", "devoured"
     ], [
             req.body.burger_name, req.body.devoured
-        ], function (result) {
+        ], function (data) {
             res.redirect("/");
         });
+});
+
+router.put("/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: true
+    }, condition, function (data) {
+        res.redirect("/");
+    });
 });
 
 module.exports = router;
