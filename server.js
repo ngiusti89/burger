bc898710ba9b55854e6440aa545b5cd0fecf5fdb
Natasha Giusti <natashagiusti@tashs-mac.lan>
2019-06-01 17:46:48 -0700

    updated server, error

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var exHandBars = require("express-handlebars");
+var routes = require("./controllers/burgers_controller.js");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exHandBars({defaultLayout: "main"}));
+
+app.set("view engine", "handlebars");
+
+app.use("/", routes);
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT " + PORT);
+});
